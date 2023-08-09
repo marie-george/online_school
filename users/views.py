485680338c024cc62755476471fb21cfec6c7c06
marie-george <maria85night@gmail.com>
2023-08-09 03:13:03 +0300

    views and serializer for User

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
