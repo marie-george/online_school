9b279d72ed029aa3409b174a6791ba67db200b28
marie-george <maria85night@gmail.com>
2023-08-14 23:46:43 +0300

    lessons and lessons count for courses

--- school/serializers.py
+++ school/serializers.py
@@ -3,13 +3,29 @@ from rest_framework import serializers
 from school.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
+
+    def get_lessons_count(self, lesson):
+        lessons = Lesson.objects.filter(course=lesson).all()
+        if lessons:
+            return lessons.count()
+        return 0
+
+    class Meta:
+        model = Course
+        fields = (
+            'name',
+            'preview',
+            'description',
+            'lessons_count',
+            'lessons'
+        )
+
