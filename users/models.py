42b13c5ed179ba7ef8284040ebc226a37b7972fd
marie-george <maria85night@gmail.com>
2023-08-15 21:33:55 +0300

    corrections to user roles

--- users/models.py
+++ users/models.py
@@ -4,17 +4,18 @@ from django.db import models
 NULLABLE = {'null': True, 'blank': True}
 
 
+class UserRoles(models.TextChoices):
+    CLIENT = 'client', 'обычный пользователь'
+    MODERATOR = 'moderator', 'модератор'
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     city = models.CharField(max_length=50, verbose_name='город')
-    roles = (
-        ('client', 'обычный пользователь'),
-        ('moderator', 'модератор'),
-    )
-    role = models.CharField(choices=roles, default='moderator', verbose_name='роль')
+    role = models.CharField(choices=UserRoles.choices, default=UserRoles.CLIENT, verbose_name='роль')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
