2f7c4b27dac3ac84e5a5cf42b0875fc158a26d66
marie-george <maria85night@gmail.com>
2023-08-15 21:31:57 +0300

    password validalion

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        instance.is_active = True
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
