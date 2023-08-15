3fe3d431ef9a00506901924d730591935133158b
marie-george <maria85night@gmail.com>
2023-08-15 18:38:58 +0300

    added permissions IsAuthenticated

--- school/views.py
+++ school/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics, filters
+from rest_framework import viewsets, generics, filters, permissions
 
 from school.models import Course, Lesson, Payment
 from school.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
@@ -7,37 +7,45 @@ from school.serializers import CourseSerializer, LessonSerializer, PaymentSerial
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['course__name', 'payment_method']
     ordering_fields = ['payment_date']
+
 
 # сортировка по дате оплаты:
 #   прямой порядок - http://localhost:8000/school/payments/?ordering=payment_date
