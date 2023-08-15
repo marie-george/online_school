f5dacb6a71d2deddae67f814a799ee38447da027
marie-george <maria85night@gmail.com>
2023-08-15 18:39:53 +0300

    added roles to User

--- users/models.py
+++ users/models.py
@@ -10,6 +10,11 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     city = models.CharField(max_length=50, verbose_name='город')
+    roles = (
+        ('client', 'обычный пользователь'),
+        ('moderator', 'модератор'),
+    )
+    role = models.CharField(choices=roles, default='moderator', verbose_name='роль')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
